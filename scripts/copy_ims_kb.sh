5d953bec82b58cd60ecf45f5e4c0424ead9be185
KovalM <michail.kovalev217@gmail.com>
2017-02-12 19:24:11 +0300

    Remove OOTB agents from copy_ims_kb

--- scripts/copy_ims_kb.sh
+++ scripts/copy_ims_kb.sh
@@ -12,9 +12,6 @@ cp -a ims.ostis.kb/ims/ostis_tech/semantic_network_represent/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/ims/ostis_tech/unificated_models/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/ims/ostis_tech/semantic_networks_processing/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/ims/ostis_tech/lib_ostis/sectn_lib_of_reusable_comp_ui/ui_menu/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/ims/ostis_tech/lib_ostis/sectn_lib_reusable_comp_kpm/reusable_sc_agents/lib_c_agents/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/ims/ostis_tech/lib_ostis/sectn_lib_reusable_comp_kpm/reusable_sc_agents/lib_scp_agents/ ims.ostis.kb_copy/
-cp -a ims.ostis.kb/ims/ostis_tech/lib_ostis/sectn_lib_reusable_comp_kpm/programs_for_sc_text_processing/scp_program/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/to_check/ ims.ostis.kb_copy/
 cp -a ims.ostis.kb/ui/ ims.ostis.kb_copy/
 rm -rf ims.ostis.kb_copy/ui/menu
