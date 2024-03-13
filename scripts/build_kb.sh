94b5a75e9482ab9f349804cadafb44cc9810d8e9
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-03-13 22:34:54 +0300

    build: don't copy ims.ostis.kb

--- scripts/build_kb.sh
+++ scripts/build_kb.sh
@@ -2,9 +2,4 @@
 set -eo pipefail
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 
-"${DM_ROOT_PATH}/scripts/copy_ims_kb.sh"
-
-if [ ! -d "${DM_ROOT_PATH}/kb.bin" ]; then
-    mkdir "${DM_ROOT_PATH}/kb.bin"
-fi
 "${DM_ROOT_PATH}/sc-machine/scripts/build_kb.sh" -i "${DM_ROOT_PATH}/repo.path"
