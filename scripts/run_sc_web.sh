dca7921f635455add8a810290c619c2d19c21f26
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2025-04-23 18:58:08 +0300

    scripts: add default event wait time for sc-web

--- scripts/run_sc_web.sh
+++ scripts/run_sc_web.sh
@@ -2,4 +2,4 @@
 set -eo pipefail
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
 
-source "${SC_WEB_PATH}/.venv/bin/activate" && python3 "${SC_WEB_PATH}/server/app.py" "$@"
+source "${SC_WEB_PATH}/.venv/bin/activate" && python3 "${SC_WEB_PATH}/server/app.py" --event-wait-timeout=50 "$@"
